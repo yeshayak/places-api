8042a40a4db74a24b6ca3d31752f764237924b73
Yeshaya Kohn <shayackohn@gmail.com>
2024-12-24 16:37:53 -0500

    update script type to module, add async loading for Google Maps API, and bump version to 0.3.13

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const injectScript = (file_path, tag) => {
   const node = document.getElementsByTagName(tag)[0]
   const script = document.createElement('script')
-  script.setAttribute('type', 'text/javascript')
+  script.setAttribute('type', 'module')
   script.setAttribute('src', file_path)
   node.appendChild(script)
 }
--- loadMap.js
+++ loadMap.js
@@ -1,6 +1,6 @@
 // Create the script tag, set the appropriate attributes
 var script = document.createElement('script')
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCYZ3Lh7jBsntbLSL31QqVtC3RVyYEHr9w&libraries=places&callback=initMap'
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCYZ3Lh7jBsntbLSL31QqVtC3RVyYEHr9w&loading=async&libraries=places&callback=initMap'
 script.defer = true
 
 // Attach your callback function to the `window` object

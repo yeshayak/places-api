d5020c99d7fee31a602be11d552ed25baf0c1e11
Yeshaya Kohn <shayackohn@gmail.com>
2025-04-01 00:07:23 -0400

    chore: remove unused image assets and add Angular dependencies

--- background.js
+++ background.js
@@ -16,33 +16,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
     console.log(tabInfo)
   }
 })
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.action === 'postMessage') {
-//     const myHeaders = new Headers()
-//     myHeaders.append('Content-Type', 'application/json')
-//     myHeaders.append('Authorization', 'Basic WjRPQkpMSDRFVkNWMldJV0lOR0g2UVY1NzRXVTVUVE06WXVGOGJFbTFUYVpiSkM1YmRyOXpjcVFhWUltSVVDR3h2aGJBSTdoRWxUQQ==')
-
-//     const raw = JSON.stringify({
-//       text: 'Your package is ready for pickup!',
-//       from: '+17326554339',
-//       to: '+18483738096',
-//       status_callback: 'https://www.toptal.com/developers/postbin/abc-123',
-//       tags: ['order_pickup'],
-//     })
-
-//     const requestOptions = {
-//       method: 'POST',
-//       headers: myHeaders,
-//       body: raw,
-//       redirect: 'follow',
-//     }
-
-//     fetch('https://gatorps.prokeep.com/rest/v1/messages', requestOptions)
-//       .then((response) => response.text())
-//       .then((result) => sendResponse({ result }))
-//       .catch((error) => sendResponse({ error }))
-
-//     return true // Will respond asynchronously
-//   }
-// })
